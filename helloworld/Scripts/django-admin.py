36ec8c46e19b8efa21b1b09fe443a0df09c6c2bc
Julia <riyamtechcampus@gmail.com>
2020-07-25 13:28:32 +0300

    react django

---
+++ helloworld/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\2020\django-apps\react-django\denis-reactdjango\helloworld\helloworld\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
